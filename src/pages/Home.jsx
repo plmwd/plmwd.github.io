81c634c13813e821159bfff0dc6cf3127fa2db6e
Paul Wood <pmwood@pm.me>
2023-06-19 20:15:02 -0700

    yeet

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import Hero from "../components/Hero";
-
-export default function Home() {
-  return (
-    <div className="page page-content">
-      <Hero title="Paul Wood" description="Full-stack Embedded IoT developer" image="/images/me.jpeg"/>
-    </div>
-  );
-}
